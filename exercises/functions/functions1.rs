a4b59844fee85270b5edce8b259a2762245671d3
Noah May <noahmouse2011@gmail.com>
2023-04-29 02:48:31 -0500

    functions1

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,7 +1,11 @@
 // functions1.rs
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+fn call_me() {
+    // I tried being clever by using the todo!() macro, but I forgot it panics when you call the function.
+    // Derp...
+    // todo!();
+}
 
 fn main() {
     call_me();
