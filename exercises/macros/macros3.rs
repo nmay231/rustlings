b8f88d0e7b08ae8d33d84d40a7b551d733631006
Noah May <noahmouse2011@gmail.com>
2023-05-12 19:03:36 -0500

    macros3

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,15 +2,18 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
 }
+
+// Perhaps #[macro_use] is provided by default for local mod {} ? I mean, it is "imported" automatically as well (you don't need `use macros`).
+// #[macro_use]
+// use macros::*;
 
 fn main() {
     my_macro!();
